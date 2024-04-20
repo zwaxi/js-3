a6c4554fc20dcd1e1051d9f7c9dbf9d951a9c525
zwaxi <muhammadishkuvatov@gmail.com>
2024-04-20 13:35:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let random = Math.random();
+let multiplication = 2 * random
+let division = random / 2
+
+alert("random =" + random + "\nmultiplication =" + multiplication  + "\ndivision =" + division )
